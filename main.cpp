951ab47b4efddc5469eaab09d9b83ac79e194f6d
Tad Keller <logischgl@gmail.com>
2019-03-18 19:40:09 -0500

    added validation layers

--- main.cpp
+++ main.cpp
@@ -23,6 +23,86 @@ const std::vector<const char*> validationLayers =
 
 GLFWwindow* window;
 VkInstance instance;
+VkDebugUtilsMessengerEXT debugMessenger;
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator)
+{
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+
+    if (func != nullptr)
+    {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
+{
+    std::cerr<<"validation layer: "<<pCallbackData->pMessage<<std::endl;
+    return VK_FALSE;
+}
+
+//this function actually creates the messenger with creation info.
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
+{
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+
+    if (func != nullptr)
+    {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    else
+    {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+//basically what is says, setup the Debug messenger than create it with a function call to "CreateDebugUtilsMessengerEXT"
+void setupDebugMessenger()
+{
+    //don't waste time calling the rest of the function if validation layers are turned off
+    if(!enableValidationLayers) return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+    //telling vulkan what kind of struct this is.
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    //specifying the message severities that we would like to be warned about.
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    //same as messageSeverity, but with the type of message.
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    //giving the messenger a function pointer to our callback function
+    createInfo.pfnUserCallback = debugCallback;
+    //doesn't have to be here.
+    createInfo.pUserData = nullptr;
+
+    if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to set up debug messenger!");
+    }
+
+}
+
+//this function returns a vector with all of the required extensions we need for the program.
+std::vector<const char*> getRequiredExtensions()
+{
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    //this gets how many extensions glfw needs for vulkan to work with it.
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    //check wtf is happening here
+    //now we put the needed glfw vulkan extensions in the vector of extensions
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    //if we are using validation layers, push the validation layer extension in the extension vector.
+    if(enableValidationLayers)
+    {
+        //this pushes back the extension we need for validation layers, it is just a macro with a string.
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
+}
 
 //this function checks if the layers we want specified in the "validationLayers" vector is actually supported
 bool checkValidationLayerSupport()
@@ -91,43 +171,24 @@ void createInstance()
         //actually give vulkan the validation layers we want to use.
         createInfo.ppEnabledLayerNames = validationLayers.data();
     }
+    else
+        createInfo.enabledLayerCount = 0;
 
-
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
-
-    //this function gets all of the required vulkan extensions glfw needs to create a window surface.
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
-    createInfo.enabledLayerCount = 0;
+    //finally give vulkan the extensions we want to use.
+    std::vector<const char*> extensions = getRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
 
     if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
         throw std::runtime_error("Vulkan instance failed to create.");
     }
     else
         std::clog<<"Vulkan instance created!"<<std::endl;
-
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::vector<VkExtensionProperties> extensions(extensionCount);
-
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-    std::cout<<"extensions : "<<std::endl;
-
-    for(const auto& extension : extensions)
-    {
-        std::cout<<extension.extensionName<<std::endl;
-    }
-
 }
 void initVulkan()
 {
     createInstance();
+    setupDebugMessenger();
 }
 
 //the main program loop
@@ -142,6 +203,10 @@ void mainLoop()
 //cleanup when the program exits, (delete vulkan objects and destroy windows)
 void cleanup()
 {
+    if (enableValidationLayers) {
+        DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    }
+
     vkDestroyInstance(instance, nullptr);
 
     glfwTerminate();
